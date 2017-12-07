3c03134bd9c455a2f9fb2d46d580735e0ed4726e
NorthwooDzp <northwood@list.ru>
2017-12-07 21:11:12 +0200

    B5L1 Using built-in pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,27 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Создание своей директивы';
-  items = [1, 2, 3, 4, 5];
-  current = 0;
-  onClick(num: number) {
+  /* title = 'Создание своей директивы';
+   items = [1, 2, 3, 4, 5];
+   current = 0;*/ // for directives
+
+  name = 'Vladimir';
+  pi = Math.PI;
+  money = 400;
+  myDate = new Date();
+  amount = 0.4585632;
+  object = {
+    foo: 'bar',
+    baz: 'qux',
+    nested: {
+      xyz: 3,
+      numbers: [1, 2, 3]
+    }
+  };
+
+  /*onClick(num: number) {
     this.current = num;
     // console.log(this.current);
-  }
+  }*/ // for directives
+
 }
