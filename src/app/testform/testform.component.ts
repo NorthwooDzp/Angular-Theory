e0ee30dee853a9d0d06756b26ec25bc045fbf5f9
NorthwooDzp <northwood@list.ru>
2017-12-12 22:56:37 +0200

    B7L3 TD validation part1

--- src/app/testform/testform.component.ts
+++ src/app/testform/testform.component.ts
@@ -7,19 +7,23 @@ import {NgForm} from "@angular/forms";
   styleUrls: ['./testform.component.scss']
 })
 export class TestformComponent implements OnInit {
-
   answers: [{ text: string, value: string }] = [
     {text: 'Нет', value: 'no'},
     {text: 'Да', value: 'yes'}
   ]
-
+  formstatus: boolean = true;
 
   constructor() {
   }
 
+
+  // this.formstatus = (this.form.form.status === 'INVALID') ? true : false;
+
   ngOnInit() {
   }
+
   submitForm(event: Event, form: NgForm) {
     console.log(form);
+
   }
 }
