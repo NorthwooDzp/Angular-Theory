a451762a5630e91487fbd3ad23aa2a907ae3c2d2
NorthwooDzp <northwood@list.ru>
2017-12-14 18:24:17 +0200

    B7L8 R-Form

--- src/app/testform/testform.component.ts
+++ src/app/testform/testform.component.ts
@@ -8,7 +8,7 @@ import {LoggerService} from '../services/logger.service';
   styleUrls: ['./testform.component.scss']
 })
 export class TestformComponent implements OnInit {
-  @ViewChild('form') form2: NgForm;
+  @ViewChild('form') form: NgForm;
 
   answers: [{ text: string, value: string }] = [
     {text: 'Нет', value: 'no'},
@@ -19,35 +19,27 @@ export class TestformComponent implements OnInit {
     country: 'ua',
     answer: 'yes'
   };
-  copy = {};
   isSubmited = false;
 
   formData = {};
-
-
 
   constructor() {
   }
 
   ngOnInit() {
-
   }
 
   submitForm(event: Event) {
     this.isSubmited = true;
-    this.formData = this.form2.value;
-    for (let key in this.form2) {
-      this.copy[key] = this.form2[key];
-    }
-    console.dir(this.copy);
-    console.dir(this.form2);
-    this.form2.reset();
+    this.formData = this.form.value;
+    console.log(this.form);
+    this.form.reset();
 
   }
 
   addRE() {
     const randEmail = '1234@mail.com';
-    this.form2.form.patchValue({
+    this.form.form.patchValue({
       user: {email: randEmail}
     });
   }
