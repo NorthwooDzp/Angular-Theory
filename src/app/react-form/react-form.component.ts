0b93089e4e61641bbf150e1913338dd7a3fead8b
NorthwooDzp <northwood@list.ru>
2017-12-15 14:48:40 +0200

    B7L9 R validation inputs

--- src/app/react-form/react-form.component.ts
+++ src/app/react-form/react-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-react-form',
@@ -20,8 +20,8 @@ export class ReactFormComponent implements OnInit {
 
   ngOnInit() {
     this.form = new FormGroup({
-      email: new FormControl(''),
-      password: new FormControl(''),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z_.0-9]{4,200}')]),
       country: new FormControl('ua'),
       answer: new FormControl('yes')
     });
