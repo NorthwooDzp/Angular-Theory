1809ad19a81382636c4b4f5583c7a811a3779824
NorthwooDzp <northwood@list.ru>
2017-12-15 17:10:10 +0200

    B7L12 R async validator

--- src/app/react-form/react-form.component.ts
+++ src/app/react-form/react-form.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Observable} from 'rxjs/Observable';
+import {reject} from 'q';
 
 @Component({
   selector: 'app-react-form',
@@ -19,16 +21,11 @@ export class ReactFormComponent implements OnInit {
   constructor() {
   }
 
-  logStatus() {
-    console.dir(this.form.get('user.password').errors);
-    console.log(this.form.get('user.password').valid);
-  }
-
   ngOnInit() {
     this.form = new FormGroup({
       user: new FormGroup({
-        email: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z_0-9]+@[a-zA-Z]+?\.[a-zA-Z]{2,6}')]),
-        password: new FormControl('', [Validators.required, Validators.pattern(`[a-zA-Z_.0-9]{${this.minPassLength},200}`)]),
+        email: new FormControl('', [Validators.required, Validators.email], [this.checkForEmail]),
+        password: new FormControl('', [Validators.required, Validators.pattern("[a-zA-Z_.0-9]+"), this.checkPassLength.bind(this)]),
       }),
       country: new FormControl('ua'),
       answer: new FormControl('yes')
@@ -40,8 +37,33 @@ export class ReactFormComponent implements OnInit {
     this.isSubmited = true;
   }
 
+  checkPassLength(control: FormControl) {
+    if (control.value.length < this.minPassLength) {
+      return {
+        'lengthErr': true
+      };
+    }
+    return null;
+  }
+
+  checkForEmail(control: FormControl): Promise<any> | Observable<any> {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'northwood@list.ru') {
+          resolve({
+            'emailErr': true
+          });
+        } else {
+          resolve(null);
+        }
+      }, 3000);
+    });
+  }
+
+
 
 }
 
 /*'[a-zA-Z_0-9]+@[a-zA-Z]+?\.[a-zA-Z]{2-6}'*/
 /*'[a-zA-Z_0-9]+@[a-zA-Z]+?\.[a-zA-Z]{2,6}'*/
+/**/
